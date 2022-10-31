c0a390dbc89ca2e1ed02f1928cf921337dfcceeb
Shubhansh18 <shubhanshgupta731@gmail.com>
2022-10-31 14:05:15 +0530

    Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Pagination\Paginator;
+use App\service\MyPagination;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
@@ -12,33 +12,26 @@ class UserController extends Controller
 {
     public function index(User $user, Request $request)
     {
-        $userData = $user->where(function ($query) use ($request) {
-            if($request->has('name')){
-                $query->where('name','LIKE', "%{$request->name}%");
-            }
-            if($request->has('email')){
-                $query->where('email','LIKE', "%{$request->email}%");
-            }
-            if($request->has('mobile')){
-                $query->where('mobile','LIKE', "%{$request->mobile}%");
-            }
-            if($request->has('address')){
-                $query->where('address','LIKE', "%{$request->address}%");
-            }
-        })->get();
+        $orderBy = $request->has('order')? $request->order: "ASC";
+        $userData = $user::where(function ($q) use($request){
+            $q->where('name','LIKE', "%{$request->name}%");
+            $q->where('email','LIKE', "%{$request->email}%");
+            $q->where('mobile','LIKE', "%{$request->mobile}%");
+            $q->where('address','LIKE', "%{$request->address}%");
+        })
+        ->orderBy($request->has('sortBy')? $request->sortBy: 'id', $orderBy)->get();
+        
+        $collection = new MyPagination($userData);
         if($userData->isEmpty())
         {
             return response()->json([
                 "message" => "No such user exists"
             ]);
         }
-        if($request->has('sortBy'))
-        {
-           $users = $userData->sortBy($request->sortBy)->all(); 
-           echo(count($users));
-           return Response::json($users, 200, array(), JSON_PRETTY_PRINT);
+        else{
+            $page = count($userData) < 100 ? 25 : 100;
+            $paginatedPage = $collection->paginate($page);
+            return Response::json($paginatedPage, 200, array(), JSON_PRETTY_PRINT);
         }
-        // echo(count($userData));
-        return Response::json($userData, 200, array(), JSON_PRETTY_PRINT);
     }
 }
